4ce0ed731f1b45f1320261318fba02e910f2e790
kumolonimbus <phoebe.chua@gmail.com>
2020-07-11 20:06:33 +0800

    separate handler, db and main execution code

---
+++ main.py
@@ -0,0 +1,63 @@
+
+"""
+test version of conversation bot for NDP party virtual 2020
+to enter venv: source env/bin/activate
+to begin: python3 ndpBot_v1.py
+send /start to initiate the conversation
+to leave venv: deactivate
+to terminate: CTRL-C (not command!)
+
+"""
+
+import logging
+from uuid import uuid4
+
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler)
+
+from database import DBHelper
+from handlers import add_rules, add_intro, add_name, add_gender, add_age, start, cancel, add_catch_random
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+#database-related functions
+db = DBHelper()
+
+#initialize dict keys
+RULES, INTRO, NAME, GENDER, AGE = range(5)
+
+
+def main():
+    updater = Updater("1089985624:AAHp-olImnu1Q_i8wRJZlYNQb5B5dwG6vag", use_context=True)
+    dp = updater.dispatcher
+
+    #add conversation handler with states defined earlier
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            RULES: [add_rules],
+            INTRO: [add_intro],
+            NAME: [add_name],
+            GENDER: [add_gender],
+            AGE: [add_age]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    #probably add some handlers for random text a user might send
+    dp.add_handler(conv_handler)
+    dp.add_handler(add_catch_random)
+
+    logger.info('Up and running')
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
